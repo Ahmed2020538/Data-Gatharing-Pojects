ffe23e444e8f6ea969a4d3e36985df7d56ea5109
Ahmed Elsayed <ahmed.elsayed862@yahoo.com>
2022-04-02 21:52:56 +0200

    push

---
+++ 02- Web_Scrapping_Projects/06-Jumia-Scraping-Project/config.py
@@ -0,0 +1,11 @@
+org_jumia_url = "https://www.jumia.com.eg/ar"
+
+gecko_driver  = "driver/geckodriver.exe"
+
+
+# To get diff user_agent =  ( https://developers.whatismybrowser.com/useragents/explore/ )
+
+user_agent    = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"
+
+page_timeout  = 10
+
